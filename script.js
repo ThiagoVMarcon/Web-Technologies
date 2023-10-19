678accf2699673dedd64a37570fdf875c2efeff0
Thiago Vinícius Marcon <thiagovtmarcon@gmail.com>
2023-10-19 21:11:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 
-const rulesButton = document.getElementById('rules-button');
-const rulesModal = document.getElementById('rules-modal');
-const closeButton = document.getElementById('close-button');
+const instructionsButton = document.getElementById('instructions-button');
+const instructionsCloseButton = document.getElementById('instructions-close-button');
+const instructionsModal = document.getElementById('instructions-modal');
+const settingsButton = document.getElementById('settings-button');
+const settingsCloseButton = document.getElementById('settings-close-button');
+const settingsModal = document.getElementById('settings-modal');
 
-rulesButton.addEventListener('click', () => {
-    rulesModal.style.display = 'flex';
+
+instructionsButton.addEventListener('click', () => {
+    instructionsModal.style.display = 'flex';
+    settingsModal.style.display = 'none'; // Oculta o modal de settings
 });
 
-closeButton.addEventListener('click', () => {
-    rulesModal.style.display = 'none';
+instructionsCloseButton.addEventListener('click', () => {
+    instructionsModal.style.display = 'none';
 });
 
 window.addEventListener('click', (event) => {
-    if (event.target === rulesModal) {
-        rulesModal.style.display = 'none';
+    if (event.target === instructionsModal) {
+        instructionsModal.style.display = 'none';
+    }
+});
+
+settingsButton.addEventListener('click', () => {
+    settingsModal.style.display = 'flex';
+    instructionsModal.style.display = 'none'; // Oculta o modal de settings
+});
+
+settingsCloseButton.addEventListener('click', () => {
+    settingsModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === settingsModal) {
+        settingsModal.style.display = 'none';
     }
 });
 
