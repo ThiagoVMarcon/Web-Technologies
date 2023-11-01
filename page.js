7fb122155f023ed4532e7a063acb380f33c31feb
Thiago Vinícius Marcon <thiagovtmarcon@gmail.com>
2023-11-01 18:30:23 +0000

    Update page.js

--- page.js
+++ page.js
@@ -1,21 +1,20 @@
-const loginModal = document.getElementById('login-modal');
+const loginButton = document.getElementById('login-button');
+const collapsibleContent = document.getElementById('collapsible-content');
 const instructionsModal = document.getElementById('instructions-modal');
 const settingsModal = document.getElementById('settings-modal');
 
-function getUsername() {
-    let username = document.getElementById("username");
-    if (username.type === "text") {
-        user
-    }
-}
-
-
-loginModal.style.display = 'none'
+collapsibleContent.style.display = 'none';
 instructionsModal.style.display = 'none';
 settingsModal.style.display = 'none';
 
+function openCollapsible() {
+    if (collapsibleContent.style.display === 'none' || collapsibleContent.style.display === '')
+        collapsibleContent.style.display = 'block';
+    else 
+        collapsibleContent.style.display = 'none';
+}
+
 function openModal(event) {
-    event.target.id === 'login-button' ? loginModal.style.display = 'flex' : 'none';
     event.target.id === 'instructions-button' ? instructionsModal.style.display = 'flex' : 'none';
     event.target.id === 'instructions-button' ? settingsModal.style.display = 'none' : '';
     event.target.id === 'settings-button' ? settingsModal.style.display = 'flex' : 'none';
@@ -26,6 +25,12 @@ function closeModal() {
     instructionsModal.style.display = 'none';
     settingsModal.style.display = 'none';
 }
+
+// function getUsername() {
+//     let username = document.getElementById("username");
+//     if (username.type === "text") {
+//     }
+// }
 
 // Variável para controlar o jogador atual (0 para jogador 1, 1 para jogador 2)
 let currentPlayer = 0;
