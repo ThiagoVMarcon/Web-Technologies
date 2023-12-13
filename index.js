e6d32e227fc51f19d9460ed5f79679d8fcd20be5
Sérgio Cardoso <up202107918@up.pt>
2023-12-13 21:47:52 +0000

    definidas algumas constantes para o server

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const http = require('http');
+const path = require('path');
 const url = require('url');
+const fs = require('fs');
 const reg = require('./reg.js');
 const game = require('./page.js');
+const express = require('express');
+const app = express();
+
+app.get('/', (request,response) => {
+    readFile('./index.html', 'utf8', (err, html) => {
+        if (err) {
+            response.status(500).send(err.message)
+        }
+        response.send(html);
+    })
+});
+
+app.listen(process.env.PORT)
