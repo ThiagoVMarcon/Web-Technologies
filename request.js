9cd11e41dfafb5017ca80d5291705dbef99e7cbc
Sérgio Cardoso <up202107918@up.pt>
2023-12-14 14:14:42 +0000

    tratado o problema do login-button

--- request.js
+++ request.js
@@ -1,52 +1,114 @@
 const baseURL = "http://twserver.alunos.dcc.fc.up.pt:8008";
-/*
-function register() {
-    const data = {
-      nick: "zp",
-      password: "zp!!"
-    };
 
-    fetch(`${base}/register`,{
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-    .then(r => r.json())
-    .then(j => console.log(j))
-}
 
-register()
-*/
 
+//------------------------------------------------------------------------
 async function makeRequest(urlname, data) {
-    const url =  `http://twserver.alunos.dcc.fc.up.pt:8008/${urlname}`;
-    const config = {
-      method: 'POST', 
+  let url = `${baseURL}/${urlname}`;
+  const config = {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+          "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    };
-  
-    return fetch(url, config)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Error in the request ${config.method} ${urlname}: ${response.statusText}`);
-        }
-  
-        console.log(`Successful request:  ${config.method} ${urlname}`);
-        return response.json();
-      })
-      .catch((error) => {
-        console.error('Error in the request:', error.message);
-        throw error;
-      });
+  };
+
+  try {
+      // Debugging
+      //console.log("Data:", data);
+      //console.log("Values:", Object.values(data));
+
+      if (Object.values(data).some(value => value === undefined || value === null || value === '')) {
+          throw new Error("Error: Some arguments are omitted.");
+      }
+
+      let response = await fetch(url, config);
+      let jsonResponse = await response.json();
+
+      if (jsonResponse.error) {
+          throw new Error(`Request error: ${jsonResponse.error}`);
+      }
+
+      console.log("Successful request.");
+      return jsonResponse;
+  } catch (error) {
+      console.error("Request error:" + error.message);
+      throw error;
   }
+}
 
-
-
+//---------------------------------------------------------------------------------------
   async function registerUser() {
-    let nick = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    let responseServer = await makeRequest('register',{nick, password});
+    let nick = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let responseServer = await makeRequest("register",{nick, password});
+  }
+
+//---------------------------------------------------------------------
+  /*
+  async function joinGame(group, nick, password, size) {
+    try {
+      let result = await makeRequest('join', {
+        group: group,
+        nick: nick,
+        password: password,
+        size: size,
+      });
+  
+   
+      if (result.game) {
+        console.log("Join request successful:", result);
+        console.log("Game:", result.game);
+        if (result.observation) {
+          console.log("Observation:", result.observation);
+          return true;
+        }
+      }
+    } catch (error) {
+      console.error("Error during join request:", error.message);
+      return false;
+    }
+  }
+  */
+  
+//-------------------------------------------------------------------
+
+async function leaveGame(nick, password, game) {
+  try {
+    const result = await makeRequest("leave", {
+      nick: nick,
+      password: password,
+      game: game,
+    });
+
+    console.log("Leave request successful:", result);
+
+    if (result.observation) {
+      console.log("Observation:", result.observation);
+    }
+  } catch (error) {
+    console.error("Error during leave request:", error.message);
+  }
+}
+
+
+
+async function notifyMove(nick, password, game, move) {
+  try {
+    const result = await makeRequest("notify", {
+      nick: nick,
+      password: password,
+      game: game,
+      "move": {row, column}
+    });
+
+    console.log("Notify request successful:", result);
+
+    if (result.observation) {
+      console.log("Observation:", result.observation);
+    }
+  } catch (error) {
+    console.error("Error during notify request:", error.message);
 
   }
+}
